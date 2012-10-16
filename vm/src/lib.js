04c98e48b478e3822e355993e4301e761cc5c251
Jonatan Wallgren <jonatan.wallgren@gamesys.co.uk>
2012-10-16 14:22:47 +0100

    Added strict comparison between empty string and value in tonumber function to avoid a return if passed zero

--- vm/src/lib.js
+++ vm/src/lib.js
@@ -323,7 +323,7 @@ var luajs = luajs || {};
 		
 		tonumber: function (e, base) {
 			// TODO: Needs a more generic algorithm to check what is valid. Lua supports all bases from 2 to 36 inclusive.
-			if (e == '') return;
+			if (e === '') return;
 			
 			e = ('' + e).replace (/^\s+|\s+$/g, '');	// Trim
 			base = base || 10;
