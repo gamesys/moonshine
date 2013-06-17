eeba81bfd20b94f4b9c33b7c89fc39171196842c
Paul Cuthbertson <paul.cuthbertson@gamesys.co.uk>
2013-06-17 18:00:03 +0100

    utils.toObject updated to handle refactor of arrays. Introduced utils.coerce.

--- vm/src/utils.js
+++ vm/src/utils.js
@@ -11,57 +11,90 @@ var luajs = luajs || {};
 luajs.debug = {};
 
 
+(function () {
+	var FLOATING_POINT_PATTERN = /^[-+]?[0-9]*\.?([0-9]+([eE][-+]?[0-9]+)?)?$/;
 
-luajs.utils = {
-	
-	toObject: function (table) {
-		var isArr = luajs.lib.table.getn (table) > 0,
-			result = isArr? [] : {},
-			i;
-		
-		for (i in table) {
-			if (table.hasOwnProperty (i) && !(i in luajs.Table.prototype) && i !== '__luajs') {
-					result[isArr? i - 1 : i] = ((table[i] || {}) instanceof luajs.Table)? luajs.utils.toObject (table[i]) : table[i];
+
+	luajs.utils = {
+
+
+		coerce: function (val, type, errorMessage) {
+			var n;
+
+			switch (type) {
+				case 'boolean':
+					return !(val === false || val === undefined);
+
+				case 'string':
+					return '' + val;
+
+				case 'number':
+					if (('' + val).match(FLOATING_POINT_PATTERN)) n = parseFloat(val);
+					if (n === undefined && errorMessage) throw new luajs.Error(errorMessage);
+					return n;
+
+				default:
+					throw new ReferenceError('Can not coerce to type: ' + type);
 			}
-		}
-		
-		return result;
-	},
-	
-	
-	
-	
-	parseJSON: function (json) {
+		},
 
-		var convertToTable = function (obj) {
-			for (var i in obj) {
 
-				if (typeof obj[i] === 'object') {
-					obj[i] = convertToTable (obj[i]);
-					
-				} else if (obj[i] === null) {
-					obj[i] = undefined;
-				}				
+		toObject: function (table) {
+			var isArr = luajs.lib.table.getn (table) > 0,
+				result = isArr? [] : {},
+				numValues = table__luajs.numValues,
+				i,
+				l = numValues.length;
+
+			for (i = 1; i < l; i++) {
+				result[i] = ((numValues || {}) instanceof luajs.Table)? luajs.utils.toObject(numValues[i]) : numValues[i];
+			}
+
+			for (i in table) {
+				if (table.hasOwnProperty (i) && !(i in luajs.Table.prototype) && i !== '__luajs') {
+					result[i] = ((table[i] || {}) instanceof luajs.Table)? luajs.utils.toObject (table[i]) : table[i];
+				}
 			}
 			
-			return new luajs.Table (obj);
-		};
-
-		return convertToTable (JSON.parse (json));
-	},
-	
-	
-
-
-	toFloat: function (x) {
-		if (x === Infinity || x === -Infinity) return x;
-
-		var FLOATING_POINT_PATTERN = /^[-+]?[0-9]*\.?([0-9]+([eE][-+]?[0-9]+)?)?$/;
-		if (!('' + x).match (FLOATING_POINT_PATTERN)) return;
+			return result;
+		},
 		
-		return parseFloat (x);
-	}
-	
+		
+		
+		
+		parseJSON: function (json) {
 
-};
+			var convertToTable = function (obj) {
+				for (var i in obj) {
 
+					if (typeof obj[i] === 'object') {
+						obj[i] = convertToTable (obj[i]);
+						
+					} else if (obj[i] === null) {
+						obj[i] = undefined;
+					}				
+				}
+				
+				return new luajs.Table (obj);
+			};
+
+			return convertToTable (JSON.parse (json));
+		},
+		
+		
+
+
+		toFloat: function (x) {
+			if (x === Infinity || x === -Infinity) return x;
+
+			var FLOATING_POINT_PATTERN = /^[-+]?[0-9]*\.?([0-9]+([eE][-+]?[0-9]+)?)?$/;
+			if (!('' + x).match (FLOATING_POINT_PATTERN)) return;
+			
+			return parseFloat (x);
+		}
+		
+
+	};
+
+
+})();
